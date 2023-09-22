3dae83f921060c47b6d155276df5a74782ecc2f8
LEEJEHEON717 <leejehun0717@naver.com>
2023-09-22 18:16:53 +0900

    java project

---
+++ ch09/src/funcinterface/calculator/CalculatorTest.java
@@ -0,0 +1,36 @@
+package funcinterface.calculator;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// Calculator 변수 선언 - add
+		Calculator add, sub, mul, div;
+		int num1 = 10, num2 = 5;
+		
+		// Calculator()를 구현 - 람다식
+		
+		// 덧셈 구현
+		add = (x, y) -> x + y;
+		int value1 = add.calculate(num1, num2);
+		System.out.println("두 수의 합: " + value1);
+		
+		// 뺄셈 구현
+		sub = (x, y) -> x + y;
+		int value2 = sub.calculate(num1, num2);
+		System.out.println("두 수의 차: " + value2);
+
+		// 곱셈 구현
+		mul = (x, y) -> x + y;
+		int value3 = mul.calculate(num1, num2);
+		System.out.println("두 수의 곱: " + value3);
+
+		// 나눗셈 구현
+		div = (x, y) -> x + y;
+		int value4 = div.calculate(num1, num2);
+		System.out.println("두 수의 나누기: " + value4);
+		
+		// Calculator()호출
+
+	}
+
+}
---
+++ ch09/src/stream/Student.java
@@ -0,0 +1,20 @@
+package stream;
+
+public class Student {
+	private String name; //이름
+	private int score;   //점수
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;			
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+}
